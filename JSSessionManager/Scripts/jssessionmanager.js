f01a0bbab3360cfc00371578ca28339e4d3a4320
Kalpesh Shirodker <kalpesh.shirodker+github@gmail.com>
2016-01-27 13:05:19 +0530

    Manage ASP.net session from Javascript

---
+++ JSSessionManager/Scripts/jssessionmanager.js
@@ -0,0 +1,79 @@
+﻿var jsSessionManager = (function () {
+    var webMethod_result;
+    function WebMethod_OnError(result, response) {
+        //debugger
+        //Dummy function for page method ajax call
+    }
+    function WebMethod_OnSuccess(result, response) {
+        //Dummy function for page method ajax call
+        webMethod_result = result.d;
+    }
+
+    function CallWebMethod(MethodName, ObjParams, isAsync, OnSuccessHandler, OnErrorHandler) {
+        var sessionManagerASMXService = "SessionManager.asmx";
+        try {
+            //Set the callback methods for success and error
+            if (OnSuccessHandler == undefined || typeof (OnSuccessHandler) == 'undefined') {
+                OnSuccessHandler = WebMethod_OnSuccess
+            }
+
+            if (OnErrorHandler == undefined || typeof (OnErrorHandler) == 'undefined') {
+                OnErrorHandler = WebMethod_OnError
+            }
+
+            //Serialize the webmethod function parameters
+            var serializedParams = '';
+            //using Json2.js; 
+
+            //you can download the file from the location mentioned in the references
+            serializedParams = JSON.stringify(ObjParams);
+
+            //Make the ajax calls
+            return $.ajax({
+                type: 'POST',
+                async: isAsync,
+                url: sessionManagerASMXService + '/' + MethodName,
+                data: serializedParams,
+                dataType: 'json',
+                contentType: 'application/json; charset=utf-8',
+                success: OnSuccessHandler,
+                error: OnErrorHandler
+            });
+        }
+        catch (e) {
+            //suppress error
+        }
+        return;
+    }
+
+    //Proxy method for getting the Session value for a given name 
+    function GetSessionValue(SessionName, OnSuccessHandler, OnErrorHandler) {
+        //set web method parameters; should be same as parameter name else the web 
+
+        //method wont be called
+        var methodParams = new Object();
+        methodParams.Name = SessionName;
+
+        CallWebMethod('GetSessionValue', methodParams, false, OnSuccessHandler, OnErrorHandler);
+
+        //get the response
+        return webMethod_result;
+    }
+
+    function SetSessionValue(SessionName, SessionValue, OnSuccessHandler, OnErrorHandler) {
+        //set web method parameters; should be same as parameter name else the web method wont be called
+        var methodParams = new Object();
+
+        methodParams.Name = SessionName;
+        methodParams.Value = SessionValue;
+
+        CallWebMethod('SetSessionValue', methodParams, false, OnSuccessHandler, OnErrorHandler);
+    }
+
+
+    return {
+        CallWebMethod: CallWebMethod,
+        SetSessionValue: SetSessionValue,
+        GetSessionValue: GetSessionValue
+    }
+})();
---
+++ JSSessionManager/SessionManager.asmx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace JSSessionManager
+{
+    /// <summary>
+    /// Summary description for SessionManager
+    /// </summary>
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class SessionManager : System.Web.Services.WebService
+    {
+        [WebMethod(EnableSession = true)]
+        public void SetSessionValue(String Name, String Value)
+        {
+            string sessionVal = String.Empty;
+
+            if (Session != null)
+            {
+                Session[Name] = Value;
+            }
+        }
+
+        //The important part is “EnableSession = true”
+        [WebMethod(EnableSession = true)]
+        public string GetSessionValue(String Name)
+        {
+            string sessionVal = String.Empty;
+            if (Session != null && Session[Name] != null)
+            {
+                sessionVal = Session[Name].ToString();
+            }
+            return sessionVal;
+        }
+    }
+}
